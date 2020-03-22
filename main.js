1e07a50f0e794efa0b0e9632577db349f46f0822
Sagar Baver <sagar.baver@globant.com>
2020-03-22 23:12:48 +0530

    Adds basic project files

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    description: "A pair of warm, fuzzy socks",
+    image: "./assets/vmSocks-blue-onWhite.jpg",
+    vueJsLink: "https://vuejs.org/",
+    onSale: true,
+    inventory: 8
+  }
+})
